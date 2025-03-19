f990dd2a5e87f9caed7a67a91712fa85f461a885
AbdullahKus <abdullahkus080@gmail.com>
2025-03-19 21:08:43 +0300

    test

---
+++ AcunMedyaPortolfyoProject/Controllers/DefaultController.cs
@@ -0,0 +1,50 @@
+﻿using AcunMedyaPortolfyoProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AcunMedyaPortolfyoProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbAcunMedyaProject1Entities2 db = new DbAcunMedyaProject1Entities2();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialTestimonial()
+        {
+            var values = db.Tbl_Testimonial.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialServices()
+        {
+            var deger = db.Tbl_Services.ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult PartialContact()
+        {
+            var values = db.Tbl_Contact.ToList();
+            return PartialView(values);
+        }
+        
+        public ActionResult PartialMessage()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult PartialMessage(Tbl_Message message)
+        {
+            db.Tbl_Message.Add(message);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
